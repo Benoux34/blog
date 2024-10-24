a2638be7c6b852909f71a5bcccbad2aa2bba9e1e
Benoit <benoitantunes34130@gmail.com>
2024-10-24 23:31:11 +0200

    feat: eyes following the mouse position

--- src/components/authentication/LeftSideAuth.tsx
+++ src/components/authentication/LeftSideAuth.tsx
@@ -1,4 +1,47 @@
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { getMousePosition } from "@/lib/getMousePosition";
+
 const LeftSideAuth = () => {
+  const { mouseX, mouseY } = getMousePosition();
+
+  const [eyePosition, setEyePosition] = useState<{
+    leftEye: {
+      x: number;
+      y: number;
+    };
+    rightEye: {
+      x: number;
+      y: number;
+    };
+  }>({
+    leftEye: { x: 0, y: 0 },
+    rightEye: { x: 0, y: 0 },
+  });
+
+  useEffect(() => {
+    const calculateEyeMovement = () => {
+      const eyeMovementRange = 5;
+      const offsetX =
+        (mouseX - window.innerWidth / 2) / (window.innerWidth / 2.5);
+      const offsetY =
+        (mouseY - window.innerHeight / 1.5) / (window.innerHeight / 2.5);
+
+      setEyePosition({
+        leftEye: {
+          x: offsetX * eyeMovementRange,
+          y: offsetY * eyeMovementRange,
+        },
+        rightEye: {
+          x: offsetX * eyeMovementRange,
+          y: offsetY * eyeMovementRange,
+        },
+      });
+    };
+
+    calculateEyeMovement();
+  }, [mouseX, mouseY]);
+
   return (
     <div className="h-full">
       <div className="absolute left-[24%] top-[25%] bg-blue-500 h-[400px] w-[200px]">
@@ -6,14 +49,26 @@ const LeftSideAuth = () => {
           {/* Right Eye */}
           <div className="absolute top-10 left-36 h-[20px] w-[20px] bg-white rounded-full">
             <div className="relative h-full w-full">
-              <div className="absolute top-2 left-2 h-[10px] w-[10px] bg-black rounded-full" />
+              <motion.div
+                className="absolute top-2 left-2 h-[10px] w-[10px] bg-black rounded-full"
+                animate={{
+                  x: eyePosition.rightEye.x,
+                  y: eyePosition.rightEye.y,
+                }}
+              />
             </div>
           </div>
 
           {/* Left Eye */}
           <div className="absolute top-10 left-20 h-[20px] w-[20px] bg-white rounded-full">
             <div className="relative h-full w-full">
-              <div className="absolute top-2 left-2 h-[10px] w-[10px] bg-black rounded-full" />
+              <motion.div
+                className="absolute top-2 left-2 h-[10px] w-[10px] bg-black rounded-full"
+                animate={{
+                  x: eyePosition.rightEye.x,
+                  y: eyePosition.rightEye.y,
+                }}
+              />
             </div>
           </div>
 
@@ -25,14 +80,26 @@ const LeftSideAuth = () => {
           {/* Left Eye */}
           <div className="absolute top-10 left-28 h-[20px] w-[20px] bg-white rounded-full">
             <div className="relative h-full w-full">
-              <div className="absolute top-2 left-2 h-[10px] w-[10px] bg-black rounded-full" />
+              <motion.div
+                className="absolute top-2 left-2 h-[10px] w-[10px] bg-black rounded-full"
+                animate={{
+                  x: eyePosition.rightEye.x,
+                  y: eyePosition.rightEye.y,
+                }}
+              />
             </div>
           </div>
 
           {/* Right Eye */}
           <div className="absolute top-10 left-20 h-[20px] w-[20px] bg-white rounded-full">
             <div className="relative h-full w-full">
-              <div className="absolute top-2 left-2 h-[10px] w-[10px] bg-black rounded-full" />
+              <motion.div
+                className="absolute top-2 left-2 h-[10px] w-[10px] bg-black rounded-full"
+                animate={{
+                  x: eyePosition.rightEye.x,
+                  y: eyePosition.rightEye.y,
+                }}
+              />
             </div>
           </div>
 
@@ -44,14 +111,26 @@ const LeftSideAuth = () => {
           {/* Left Eye */}
           <div className="absolute top-10 left-28 h-[20px] w-[20px] bg-white rounded-full">
             <div className="relative h-full w-full">
-              <div className="absolute top-2 left-2 h-[10px] w-[10px] bg-black rounded-full" />
+              <motion.div
+                className="absolute top-2 left-2 h-[10px] w-[10px] bg-black rounded-full"
+                animate={{
+                  x: eyePosition.rightEye.x,
+                  y: eyePosition.rightEye.y,
+                }}
+              />
             </div>
           </div>
 
           {/* Right Eye */}
           <div className="absolute top-10 left-20 h-[20px] w-[20px] bg-white rounded-full">
             <div className="relative h-full w-full">
-              <div className="absolute top-2 left-2 h-[10px] w-[10px] bg-black rounded-full" />
+              <motion.div
+                className="absolute top-2 left-2 h-[10px] w-[10px] bg-black rounded-full"
+                animate={{
+                  x: eyePosition.rightEye.x,
+                  y: eyePosition.rightEye.y,
+                }}
+              />
             </div>
           </div>
 
---
+++ src/lib/getMousePosition.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const getMousePosition = () => {
+  const [mousePosition, setMousePosition] = useState<{
+    mouseX: number;
+    mouseY: number;
+  }>({
+    mouseX: 0,
+    mouseY: 0,
+  });
+
+  useEffect(() => {
+    const updateMousePosition = (e: MouseEvent) => {
+      setMousePosition({
+        mouseX: e.clientX,
+        mouseY: e.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  return mousePosition;
+};
+
+export { getMousePosition };
