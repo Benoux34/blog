de2ea68d5ff164d9ba13d03f2033cdbb5df5cb76
Benoit <benoitantunes34130@gmail.com>
2024-10-25 22:18:20 +0200

    debug token middleware

--- auth.ts
+++ auth.ts
@@ -98,6 +98,16 @@ const authOptions = {
     strategy: "jwt",
     maxAge: 14 * 24 * 60 * 60,
   },
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production", // Active HTTPS en production
+      },
+    },
+  },
 } satisfies NextAuthConfig;
 
 export const { handlers, auth, signIn, signOut } = NextAuth(authOptions);
